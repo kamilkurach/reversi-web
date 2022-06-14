b9fe8cc7f37476b5da24aeb55d89fd168c535027
Kamil Kurach <kamilkurach@protonmail.com>
2022-06-14 09:55:38 +0200

    Format code and fix style reversi board

--- view/View.js
+++ view/View.js
@@ -27,10 +27,10 @@ class View {
     for (let j = 0; j < 8; j++) {
       if (j % 2 != 0) {
         for (let i = 0; i < 8; i++) {
-          
+
           if (i % 2 != 0) {
             const geometry = new THREE.BoxGeometry(1, 1, 0.01);
-            const material = new THREE.MeshBasicMaterial({ color: 0x405336  });
+            const material = new THREE.MeshBasicMaterial({ color: 0x405336 });
             const cube = new THREE.Mesh(geometry, material);
             scene.add(cube);
             cube.position.x = i;
@@ -45,57 +45,60 @@ class View {
           }
           i = i + 0.02;
         }
-        } else {
-          for (let i = 0; i < 8; i++) {
-            if (i % 2 != 0) {
-              const geometry = new THREE.BoxGeometry(1, 1, 0.01);
-              const material = new THREE.MeshBasicMaterial({ color: 0x405336 });
-              const cube = new THREE.Mesh(geometry, material);
-              scene.add(cube);
-              cube.position.x = i;
-              cube.position.y = j;
-            } else {
-              const geometry = new THREE.BoxGeometry(1, 1, 0.01);
-              const material = new THREE.MeshBasicMaterial({ color: 0x405336 });
-              const cube = new THREE.Mesh(geometry, material);
-              scene.add(cube);
-              cube.position.x = i;
-              cube.position.y = j;
-            }
-            i = i + 0.02;
+      } else {
+        for (let i = 0; i < 8; i++) {
+          if (i % 2 != 0) {
+            const geometry = new THREE.BoxGeometry(1, 1, 0.01);
+            const material = new THREE.MeshBasicMaterial({ color: 0x405336 });
+            const cube = new THREE.Mesh(geometry, material);
+            scene.add(cube);
+            cube.position.x = i;
+            cube.position.y = j;
+          } else {
+            const geometry = new THREE.BoxGeometry(1, 1, 0.01);
+            const material = new THREE.MeshBasicMaterial({ color: 0x405336 });
+            const cube = new THREE.Mesh(geometry, material);
+            scene.add(cube);
+            cube.position.x = i;
+            cube.position.y = j;
           }
+          i = i + 0.02;
         }
-        j = j+0.02
       }
-
-      const geometry = new THREE.BoxGeometry(8.6, 8.6, 0.01);
-      const material = new THREE.MeshBasicMaterial({ color: 0x805322 });
-      const cube = new THREE.Mesh(geometry, material);
-      cube.position.x = 3.58;
-      cube.position.y = 3.58;
-      cube.position.z = - 0.1;
-      scene.add(cube);
-
-      const board_geometry = new THREE.BoxGeometry(8.3, 8.3, 0.01);
-      const board_material = new THREE.MeshBasicMaterial({ color: 0x00000000 });
-      const board_cube = new THREE.Mesh(board_geometry, board_material);
-      board_cube.position.x = 3.58;
-      board_cube.position.y = 3.58;
-      board_cube.position.z = - 0.1;
-      scene.add(board_cube);
-    
-      camera.position.x = 3.5;
-      camera.position.y = 3.5;
-      camera.position.z = 6.5;
-
-      camera.rotation.x = 0;
-
-      renderer.render(scene, camera);
+      j = j + 0.02;
     }
+
+    // FRAME
+    const frame_geometry = new THREE.BoxGeometry(8.6, 8.6, 0.01);
+    const frame_material = new THREE.MeshBasicMaterial({ color: 0x805322 });
+    const frame_cube = new THREE.Mesh(frame_geometry, frame_material);
+    frame_cube.position.x = 3.58;
+    frame_cube.position.y = 3.58;
+    frame_cube.position.z = - 0.1;
+    scene.add(frame_cube);
+
+    // LINES
+    const board_geometry = new THREE.BoxGeometry(8.35, 8.35, 0.01);
+    const board_material = new THREE.MeshBasicMaterial({ color: 0x00000000 });
+    const board_cube = new THREE.Mesh(board_geometry, board_material);
+    board_cube.position.x = 3.58;
+    board_cube.position.y = 3.58;
+    board_cube.position.z = - 0.1;
+    scene.add(board_cube);
+
+    // CAMERA 
+    camera.position.x = 3.5;
+    camera.position.y = 3.5;
+    camera.position.z = 6.5;
+    camera.rotation.x = 0;
+
+    // RENDER
+    renderer.render(scene, camera);
   }
+}
 
 export { View };
- 
+
 // // chessbaord
 // for (let j = 0; j < 8; j++) {
 //   if (j % 2 != 0) {
