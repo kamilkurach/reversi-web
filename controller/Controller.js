dea7fa77d38c88014cb075f4a8896e175384e7a9
Kamil Kurach <kamilkurach@protonmail.com>
2022-06-16 18:37:29 +0200

    Test raycasting

--- controller/Controller.js
+++ controller/Controller.js
@@ -1,6 +1,9 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.141.0/build/three.module.js';
+
 class Controller {
   board;
-
+  raycaster = new THREE.Raycaster();
+  pointer = { x : 0, y : 0 };
   view;
 
   constructor(board, view) {
@@ -13,6 +16,41 @@ class Controller {
     this.board.initBaord();
     // this.view.initView();
     document.body.onload = this.view.constructBasicScene();
+    this.addRaycaster();
+  }
+
+  addRaycaster() {
+    var camera = this.view.camera;
+    var raycaster = this.raycaster;
+    var pointer = this.pointer;
+    var scene = this.view.scene;
+    var renderer = this.view.renderer;
+
+    function onPointerMove( event ) {
+
+      // calculate pointer position in normalized device coordinates
+      // (-1 to +1) for both components
+
+      pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+      pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
+
+      // update the picking ray with the camera and pointer position
+      raycaster.setFromCamera( pointer, camera );
+
+      // calculate objects intersecting the picking ray
+      var intersects = raycaster.intersectObjects( scene.children );
+
+      
+      intersects[ 0 ].object.material.color.set( 0x90EE90 );
+
+      renderer.render( scene, camera );
+
+    }
+
+    window.addEventListener( 'pointermove', onPointerMove );
+
+    // window.requestAnimationFrame(render);
   }
 }
 
