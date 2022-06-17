4426db44a4f5c672765c2d4db5377dce1849ceb1
Kamil Kurach <kamilkurach@protonmail.com>
2022-06-17 12:14:43 +0200

    Add basic board grid array

--- model/Board.js
+++ model/Board.js
@@ -1,9 +1,34 @@
 class Board {
+  boardGrid = [
+    [0, 0, 0, 0, 0, 0, 0 ,0],
+    [0, 0, 0, 0, 0, 0, 0 ,0],
+    [0, 0, 0, 0, 0, 0, 0 ,0],
+    [0, 0, 0, 0, 0, 0, 0 ,0],
+    [0, 0, 0, 0, 0, 0, 0 ,0],
+    [0, 0, 0, 0, 0, 0, 0 ,0],
+    [0, 0, 0, 0, 0, 0, 0 ,0],
+    [0, 0, 0, 0, 0, 0, 0 ,0],
+  ]
+
   constructor() {
   }
 
   initBaord() {
     console.log('init Board class');
+    this.printBoardGrid();
+  }
+
+  printBoardGrid() {
+    console.table(this.boardGrid);
+  }
+
+  getBoardGrid() {
+    return this.boardGrid;
+  }
+
+  setBoardGrid(x, y, state) {
+    this.boardGrid[x][y] = state;
+    this.printBoardGrid();
   }
 }
 
