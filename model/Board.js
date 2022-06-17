174d010eb934321affb02c7903bc252a3addc64b
Kamil Kurach <kamilkurach@protonmail.com>
2022-06-17 19:05:32 +0200

    Add find discs method

--- model/Board.js
+++ model/Board.js
@@ -1,13 +1,13 @@
 class Board {
   boardGrid = [
-    [0, 0, 0, 0, 0, 0, 0 ,0],
-    [0, 0, 0, 0, 0, 0, 0 ,0],
-    [0, 0, 0, 0, 0, 0, 0 ,0],
-    [0, 0, 0, 0, 0, 0, 0 ,0],
-    [0, 0, 0, 0, 0, 0, 0 ,0],
-    [0, 0, 0, 0, 0, 0, 0 ,0],
-    [0, 0, 0, 0, 0, 0, 0 ,0],
-    [0, 0, 0, 0, 0, 0, 0 ,0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
   ]
 
   constructor() {
@@ -28,6 +28,20 @@ class Board {
   setBoardGrid(x, y, state) {
     this.boardGrid[x][y] = state;
   }
+
+  findAllPlayersDiscs(player) {
+    let playerDiscs = [];
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (this.boardGrid[i][j] == player) {
+          let discPosition = [i, j];
+          playerDiscs.push(discPosition);
+        }
+      }
+    }
+    return playerDiscs;
+  }
+
 }
 
 export { Board };
