3ca2694bfe061e38a75d9432ba27be88fa845718
Kamil Kurach <kamilkurach@protonmail.com>
2022-06-24 21:48:41 +0200

    Update todo

--- runApp.js
+++ runApp.js
@@ -6,7 +6,6 @@ let TODO =
 "VERSION: random play (v1.0)" + '\n' + '\n' +
 "-- TODO --" + '\n' + '\n' +
 "* fix disc mesh lines click" + '\n' +
-"* make black and white discs (merge mesh)" + '\n' +
 "* fix camera" + '\n'  + 
 "* points visualizer"
 
