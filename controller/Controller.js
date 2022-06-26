9ca72930ec1f18b7e0bbed7a32042e3bd281394a
Kamil Kurach <kamilkurach@protonmail.com>
2022-06-26 04:55:13 +0200

    Format code

--- controller/Controller.js
+++ controller/Controller.js
@@ -43,7 +43,7 @@ class Controller {
     this.board.calcPoints(1);
     this.board.calcPoints(2);
     console.log("player_1_points (balck): " + this.board.player_1_points +
-     " " + "player_2_points (white): " + this.board.player_2_points);
+      " " + "player_2_points (white): " + this.board.player_2_points);
   }
 
   initDiscs() {
@@ -54,9 +54,9 @@ class Controller {
   };
 
   onPointerMove(event) {
-   
+
     this.isGameInterrupted = true;
-    
+
     if (this.board.validMoves.length != 0) {
       let raycaster = new THREE.Raycaster();
       let pointer = new THREE.Vector2();
@@ -68,14 +68,14 @@ class Controller {
 
       let intersects = raycaster.intersectObjects(this.scene.children);
 
-      for(let i = 0; i < intersects.length; i++) {
+      for (let i = 0; i < intersects.length; i++) {
         if (intersects[i].object.name == "square") {
           this.view_x = intersects[i].object.position.x
           this.view_y = intersects[i].object.position.y
           break;
         }
       }
-          
+
       this.boardGrid_x = this.view_x.toFixed(0);
       this.boardGrid_y = this.view_y.toFixed(0);
 
@@ -110,9 +110,9 @@ class Controller {
 
     } else if (this.board.validMoves.length == 0) {
       this.calcPointsForPlayers();
-      let message = 
-      "Player 1 (balck): " + this.board.player_1_points + "\n" +
-      "Player 2 (white): " + this.board.player_2_points
+      let message =
+        "Player 1 (balck): " + this.board.player_1_points + "\n" +
+        "Player 2 (white): " + this.board.player_2_points
       if (window.confirm(message)) {
         window.location.reload();
       }
@@ -145,15 +145,15 @@ class Controller {
   }
 
   randomBotMove() {
-  
+
     setTimeout(() => {
       if (this.board.validMoves.length != 0) {
 
         let pickedMove = Math.floor(Math.random() * this.board.validMoves.length);
-  
+
         this.view_x = this.board.validMoves[pickedMove][0] + this.board.validMoves[pickedMove][0] * 0.02;
         this.view_y = this.board.validMoves[pickedMove][1] + this.board.validMoves[pickedMove][1] * 0.02;
-  
+
         this.boardGrid_x = this.view_x.toFixed(0);
         this.boardGrid_y = this.view_y.toFixed(0);
 
@@ -161,31 +161,31 @@ class Controller {
           let x = move[0];
           let y = move[1];
           if (this.boardGrid_x == x && this.boardGrid_y == y) {
-  
+
             // current player
             this.addDisc(this.view_x, this.view_y, this.boardGrid_x, this.boardGrid_y, this.player);
-  
+
             this.view.removeHighlightValidMoves(this.board.validMoves, this.player);
-  
+
             //  flip disc/discs
             this.flip(this.boardGrid_x, this.boardGrid_y, this.player);
-  
+
             // change player
             this.changePlayer();
-  
+
             // new player 
             this.board.recalcBoard(this.player);
-  
+
             this.view.highlightValidMoves(this.board.validMoves, this.player);
-  
+
             this.renderer.render(this.scene, this.camera);
-            
+
             break;
           }
         };
-  
-      }  
-      
+
+      }
+
     }, 1200)
   }
 
